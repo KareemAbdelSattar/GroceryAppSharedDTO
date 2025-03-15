6d7270a916e726f67bfd546e00aee47be763c065
KareemAbdElSattar <Kareem.AS.AboElFateh@gmail.com>
2025-03-15 15:25:33 +0200

    Remove reson from init

--- Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -8,9 +8,9 @@
 
 public struct RegisterResponseDTO: Codable {
     public let error: Bool
-    public let reason: String?
+    public var reason: String? = nil
     
-    public init(error: Bool, reason: String?) {
+    public init(error: Bool, reason: String? = nil) {
         self.error = error
         self.reason = reason
     }
